6cb9a3e5d420e3b219e06e4150500cce3013eafb
mira <mira79j@gmail.com>
2020-09-01 08:21:11 +0100

    cover and service page

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,45 +1,48 @@
 module.exports = function(grunt) {
-    require('jit-grunt')(grunt);
-    grunt.initConfig({
-      jshint: {
-        files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+  require('jit-grunt')(grunt);
+  grunt.initConfig({
+    // jshint: {
+    //   files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+    //   options: {
+    //     globals: {
+    //       jQuery: true
+    //     }
+    //   }
+    // },
+    sass: {
+      dist: {
         options: {
-          globals: {
-            jQuery: true
-          }
+          sourcemap: false,
+          compress: false,
+          yuicompress: false,
+          style: 'expanded',
+        },
+        files: {
+          "src/css/index.css": "src/sass/index.scss",
+          "src/css/home.css": "src/sass/home.scss",
+          "src/css/cover.css": "src/sass/cover.scss",
+          "src/css/service.css": "src/sass/service.scss",
+          },
         }
       },
-      less: {
-        development: {
-          options: {
-            compress: true,
-            yuicompress: true,
-            sourcemap: true,
-            optimization: 2
-          },
-          files: {
-            "css/main.css": "src/less/*.less" ,
-            "css/override.css": "src/sass/*.scss"
-            },
-          }
-        },
+  
     
-      
-      watch: {
-        files: ['<%= jshint.files %>'],
-        tasks: ['jshint'],
-        styles: {
-            files: ['sass/*.scss',['less/*.less']],
-            tasks: [['sass'],['less/*.less']],
-            options: {
-              nospawn: true
-            }
+    watch: {
+      // files: ['<%= jshint.files %>'],
+      tasks: ['sass'],
+      styles: {
+          files: 'src/**/*.scss',
+          tasks: ['sass'],
+          options: {
+            nospawn: true
           }
-      }
-    });
-  
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['less', 'watch']);
-  
-  };
+        }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.registerTask('default', ['sass', 'watch']);
+
+};
